d35b7b538a5c4c60e38b15fe4f8a6717b93d1b14
Chance Sweetser <chance.sweetser@outlook.com>
2020-11-15 17:45:33 -0700

    Updated Assets for Readme

--- Weather-Tracker-2/Controllers/HomeController.cs
+++ Weather-Tracker-2/Controllers/HomeController.cs
@@ -17,13 +17,6 @@ namespace Weather_Tracker_2.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
